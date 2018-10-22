588bafad515832e22c01539656d5044340ae3e41
fgyuka <feketsgyorgy@gmail.com>
2018-10-22 13:51:11 +0200

    Update Accel.v

--- Accel.v
+++ Accel.v
@@ -3,15 +3,11 @@
 module Accel(
 	input clk,
 	input miso,
-	output [6:0] out
+	output [:0] out
     );
 
 assign out[0] = 0;	// ~CS
-assign out[1] = 0;	// MOSI
-assign out[2] = clk;	// SCLK (Osztandó)
-assign out[3] = 0;	// GND
-assign out[4] = 0;	// GND
-assign out[5] = 1;	// VCC
-assign out[6] = 1;	// VCC
+assign out[1] = 0;	// MISO
+assign out[2] = clk;	// SCLK (OsztandÃ³)
 
 endmodule
